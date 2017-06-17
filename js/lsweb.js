c21462ef25a4f125626e9d9106e245d3398109ed
Lester Vecsey <lvecsey@gmail.com>
2017-06-17 18:31:35 -0400

    start work on upvote downvote, and color in each row with alternating colors.

--- js/lsweb.js
+++ js/lsweb.js
@@ -1,10 +1,29 @@
 
-function lsweb_show_entry(entry) {
+var uparrow = '↟';
+
+var downarrow = '↡';
+
+var VoteEnum = {
+    UP: 0,
+    DOWN: 1
+};
+
+function add_clickqueue(clickval) {
+
+    var vote_str = clickval == VoteEnum.UP ? 'upvote' : 'downvote';
+    
+    console.log('Adding ' + vote_str + ' to transfer queue.');
+    
+}
+
+function lsweb_show_entry(entry, bgcolor) {
 
     var str = '';
 
-    str += '<TR><TD><a href="' + entry.url + '">' + entry.title + '</a></TD></TR>';
-    
+    str += '<TR BGCOLOR="' + bgcolor + '"><TD COLSPAN="3"><A HREF="javascript:add_clickqueue(VoteEnum.UP)">' + uparrow + '</A></TD></TR>';
+    str += '<TR BGCOLOR="' + bgcolor + '"><TD>&nbsp;</TD><TD><a href="' + entry.url + '">' + entry.title + '</a></TD></TR>';
+    str += '<TR BGCOLOR="' + bgcolor + '"><TD COLSPAN="3"><A HREF="javascript:add_clickqueue(VoteEnum.DOWN)">' + downarrow + '</A></TD></TR>';
+
     return str;
     
 }
@@ -15,11 +34,13 @@ function lsweb_draw_table(entries) {
 
     var str = '';
 
-    str += '<TABLE BORDER="0">';
+    var bgcolors = [ '#BBBBBB', '#CCCCCC' ];
+    
+    str += '<TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0">';
 
     for (n = 0; n < entries.length; n++) {
 
-	str += lsweb_show_entry(entries[n]);
+	str += lsweb_show_entry(entries[n], bgcolors[n%2]);
 
     }
 
